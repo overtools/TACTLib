b17758a97accf36b4a2e445aae812f5aab932686
Diego Rossi <git@hellpie.dev>
2020-04-11 00:33:15 +0200

    Add ProductHandler for Odin and route file handling back to ClientHandler's VFS Files Tree

---
+++ TACTLib/Core/Product/Odin/OdinProductHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using TACTLib.Client;
+
+namespace TACTLib.Core.Product.Odin {
+	[ProductHandler(TACTProduct.ModernWarfare)]
+	public class OdinProductHandler : IProductHandler {
+		private ClientHandler _client;
+
+		public OdinProductHandler(ClientHandler client, Stream stream) {
+			_client = client;
+		}
+
+		/// <inheritdoc />
+		public Stream OpenFile(object key) {
+			if (key is string fileKey) {
+				return _client.VFS.Open(fileKey);
+			}
+
+			return null;
+		}
+	}
+}
