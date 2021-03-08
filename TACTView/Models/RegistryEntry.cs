6f433aa8a3bada5d686c226e3cf7073b9cdd768d
Lilith Naomi Nightingale <yretenai@gmail.com>
2021-03-08 01:32:31 +0000

    [TACTView] monakS v2

--- TACTView/Models/RegistryEntry.cs
+++ TACTView/Models/RegistryEntry.cs
@@ -6,7 +6,7 @@ using TACTLib;
 namespace TACTView.Models {
     internal record RegistryEntry<T>(string Name, TACTProduct Product, Type Type) where T : class {
         internal T CreateInstance(ServiceCollection collection) {
-            var provider = collection.AddSingleton<T>(Type).BuildServiceProvider();
+            var provider = collection.AddSingleton(typeof(T), Type).BuildServiceProvider();
             collection.Remove(collection.Last());
             return provider.GetRequiredService<T>();
         }
