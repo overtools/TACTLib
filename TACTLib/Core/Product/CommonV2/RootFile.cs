eaee4384a4ed00dbd51dbbb03e687ed5472f171f
shalzuth <shalzuth@users.noreply.github.com>
2021-04-08 22:48:28 -0700

    [osi] Diablo II Remastered support

--- TACTLib/Core/Product/CommonV2/RootFile.cs
+++ TACTLib/Core/Product/CommonV2/RootFile.cs
@@ -11,7 +11,7 @@ namespace TACTLib.Core.Product.CommonV2 {
         public byte MPriority;
         public string FileName;
         public string InstallPath;
-
+        
         public RootFile(IReadOnlyList<string> data) {
             FileID = data[0];
             MD5 = CKey.FromString(data[1]);
@@ -20,6 +20,10 @@ namespace TACTLib.Core.Product.CommonV2 {
             MPriority = byte.Parse(data[4]);
             FileName = data[5];
             InstallPath = data[6];
+        }
+        public RootFile(string fileName, string md5)  {
+            FileName = fileName;
+            MD5 = CKey.FromString(md5);
         }
     }
 }
