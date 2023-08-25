b229f0678b9e867d63b1db8f27223cc3fb391b4c
zingballyhoo <zingballyhoo@live.co.uk>
2023-08-25 23:05:01 +0100

    implement archive group paging

--- TACTLib/Protocol/CDNIndexHandler.cs
+++ TACTLib/Protocol/CDNIndexHandler.cs
@@ -1,6 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
+using System.Runtime.InteropServices;
+using CommunityToolkit.HighPerformance;
+using Microsoft.Win32.SafeHandles;
 using TACTLib.Client;
 using TACTLib.Container;
 using TACTLib.Core.Key;
@@ -15,37 +19,17 @@ namespace TACTLib.Protocol
         public uint Size;
     }
 
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
     public class CDNIndexHandler
     {
-        private readonly ClientHandler m_client;
+        private readonly ClientHandler Client;
         private readonly Dictionary<FullKey, IndexEntry> CDNIndexData = new Dictionary<FullKey, IndexEntry>(CASCKeyComparer.Instance);
 
-        //private byte[][] ArchiveGroupPages = Array.Empty<byte[]>();
-        //private FullKey[] ArchiveGroupPageLastEKeys = Array.Empty<FullKey>();
-        // todo: paging
+        private FixedFooter ArchiveGroupFooter;
+        private SafeFileHandle ArchiveGroupFileHandle = new SafeFileHandle();
+        private FullKey[] ArchiveGroupPageLastEKeys = Array.Empty<FullKey>();
 
         private const int ARCHIVE_ID_GROUP = -1;
-        
-        private CDNIndexHandler(ClientHandler client)
-        {
-            m_client = client;
-
-            var archiveGroupHash = client.ConfigHandler.CDNConfig.Values["archive-group"][0];
-            if (m_client.ContainerHandler != null && OpenIndexFile(archiveGroupHash, ARCHIVE_ID_GROUP)) {
-                // no need to load individual indices
-                return;
-            }
-            
-            for (var i = 0; i < client.ConfigHandler.CDNConfig.Archives.Count; i++)
-            {
-                string archive = client.ConfigHandler.CDNConfig.Archives[i];
-
-                if (m_client.ContainerHandler != null)
-                    OpenOrDownloadIndexFile(archive, i);
-                else
-                    DownloadIndexFile(archive, i);
-            }
-        }
 
         public static CDNIndexHandler Initialize(ClientHandler clientHandler)
         {
@@ -53,11 +37,63 @@ namespace TACTLib.Protocol
             return handler;
         }
         
+        private CDNIndexHandler(ClientHandler client)
+        {
+            Client = client;
+
+            var archiveGroupHash = client.ConfigHandler.CDNConfig.Values["archive-group"][0];
+            if (LoadGroupIndexFile(archiveGroupHash)) {
+                // new paging impl
+                return;
+            }
+            
+            // using shared impl...
+            //if (m_client.ContainerHandler != null && OpenIndexFile(archiveGroupHash, ARCHIVE_ID_GROUP)) {
+            //    // no need to load individual indices
+            //    return;
+            //}
+            
+            for (var index = 0; index < client.ConfigHandler.CDNConfig.Archives.Count; index++)
+            {
+                string archive = client.ConfigHandler.CDNConfig.Archives[index];
+                OpenOrDownloadIndexFile(archive, index);
+            }
+        }
+
+        private bool LoadGroupIndexFile(string hash) {
+            if (Client.ContainerHandler == null) return false; // file only exists locally
+            
+            var path = GetArchiveIndexPath(hash);
+            if (!File.Exists(path)) return false;
+
+            ArchiveGroupFileHandle = File.OpenHandle(path);
+            var length = (int)RandomAccess.GetLength(ArchiveGroupFileHandle);
+
+            var maxFooterSize = FixedFooter.SIZE + FixedFooter.MAX_CHECKSUM_SIZE; // (we only care about the end checksum, not start)
+            var maxFooterBuffer = new byte[maxFooterSize];
+            RandomAccess.Read(ArchiveGroupFileHandle, maxFooterBuffer, length-maxFooterSize);
+            
+            using (var maxFooterReader = new BinaryReader(new MemoryStream(maxFooterBuffer))) {
+                ArchiveGroupFooter = ReadFooter(maxFooterReader);
+            }
+
+            GetTableParameters(ArchiveGroupFooter, length, out var pageSize, out var pageCount);
+            
+            if (ArchiveGroupFooter.m_sizeBytes != 4) throw new Exception($"invalid `size` size for group: {ArchiveGroupFooter.m_sizeBytes} (expected 4)");
+            if (ArchiveGroupFooter.m_offsetBytes != 6) throw new Exception($"invalid `offset` size for group: {ArchiveGroupFooter.m_offsetBytes} (expected 6)");
+            
+            var lastEKeyArrayOffset = pageCount * pageSize;
+            ArchiveGroupPageLastEKeys = new FullEKey[pageCount];
+            RandomAccess.Read(ArchiveGroupFileHandle, ArchiveGroupPageLastEKeys.AsSpan().AsBytes(), lastEKeyArrayOffset);
+
+            return true;
+        }
+        
         private static FixedFooter ReadFooter(BinaryReader br) {
-            var footerHashSize = 16;
+            var footerHashSize = FixedFooter.MAX_CHECKSUM_SIZE;
 
             while (true) {
-                if (footerHashSize < 8) throw new Exception("unable to determine footer hash size");
+                if (footerHashSize < FixedFooter.MIN_CHECKSUM_SIZE) throw new Exception("unable to determine footer hash size");
 
                 br.BaseStream.Position = br.BaseStream.Length - footerHashSize - FixedFooter.SIZE;
                 var footer = br.Read<FixedFooter>();
@@ -76,7 +112,7 @@ namespace TACTLib.Protocol
         }
 
         private static void GetTableParameters(FixedFooter footer, int length, out int pageSize, out int pageCount) {
-            pageSize = footer.m_blockSizeKB * 1024;
+            pageSize = footer.PageSizeBytes;
             var totalSizeForPage = pageSize + footer.m_keyBytes + footer.m_checksumSize; // every page will have lastEKey + hash
             pageCount = (length - footer.DynamicSize) / totalSizeForPage;
             
@@ -113,7 +149,7 @@ namespace TACTLib.Protocol
 
                     uint size;
                     if (footer.m_sizeBytes == 4) size = SpanHelper.ReadStruct<UInt32BE>(ref pageSpan).ToInt();
-                    else throw new Exception($"unhanled `size` size: {footer.m_sizeBytes}");
+                    else throw new Exception($"unhandled `size` size: {footer.m_sizeBytes}");
 
                     ushort entryArchiveIndex = (ushort)archiveIndex;
                     if (archiveIndex == ARCHIVE_ID_GROUP) {
@@ -122,7 +158,7 @@ namespace TACTLib.Protocol
                     
                     uint offset;
                     if (footer.m_offsetBytes == 4) offset = SpanHelper.ReadStruct<UInt32BE>(ref pageSpan).ToInt();
-                    else throw new Exception($"unhanled `offset` size: {footer.m_offsetBytes}");
+                    else throw new Exception($"unhandled `offset` size: {footer.m_offsetBytes}");
                     
                     var entry = new IndexEntry
                     {
@@ -150,7 +186,7 @@ namespace TACTLib.Protocol
         {
             try
             {
-                var cdn = (CDNClient) m_client.NetHandle!;
+                var cdn = (CDNClient) Client.NetHandle!;
                 var indexData = cdn.FetchCDN("data", archive, null, ".index");
                 if (indexData == null) throw new Exception($"failed to fetch archive index data for {archive} (index {i})");
                 using var indexDataStream = new MemoryStream(indexData);
@@ -163,12 +199,13 @@ namespace TACTLib.Protocol
         }
 
         private string GetArchiveIndexPath(string archive) {
-            var dir = ((ContainerHandler)m_client.ContainerHandler!).ContainerDirectory;
+            var dir = ((ContainerHandler)Client.ContainerHandler!).ContainerDirectory;
             var path = Path.Combine(dir, ContainerHandler.CDNIndicesDirectory, archive + ".index");
             return path;
         }
 
         private bool OpenIndexFile(string archive, int archiveIndex) {
+            if (Client.ContainerHandler == null) return false;
             var path = GetArchiveIndexPath(archive);
             if (!File.Exists(path)) return false;
             
@@ -191,36 +228,95 @@ namespace TACTLib.Protocol
             }
         }
 
-        public bool TryGetIndexEntry(FullKey ckey, out IndexEntry indexEntry) {
-            return CDNIndexData.TryGetValue(ckey, out indexEntry);
+        private bool TryGetIndexEntryFromGroup(FullEKey eKey, out IndexEntry indexEntry) {
+            //if (ArchiveGroupFileHandle.IsInvalid) {
+            //    indexEntry = default;
+            //    return false;
+            //}
             
-            //indexEntry = default;
-            //return false;
+            var binarySearchResult = Array.BinarySearch(ArchiveGroupPageLastEKeys, eKey);
+            int pageIndex;
+            if (binarySearchResult >= 0) {
+                pageIndex = binarySearchResult;
+            } else {
+                var firstElementLarger = ~binarySearchResult;
+                pageIndex = firstElementLarger;
+            }
+            if (pageIndex >= ArchiveGroupPageLastEKeys.Length) goto NOT_FOUND;
+            
+            Span<byte> pageData = stackalloc byte[ArchiveGroupFooter.PageSizeBytes];
+            var pageOffset = ArchiveGroupFooter.PageSizeBytes * pageIndex;
+            RandomAccess.Read(ArchiveGroupFileHandle, pageData, pageOffset);
+
+            ReadOnlySpan<ArchiveGroupEntry> pageEntries = MemoryMarshal.Cast<byte, ArchiveGroupEntry>(pageData);
+            var speculativeEntry = new ArchiveGroupEntry {
+                m_eKey = eKey
+            };
+            var finalSearchResult = pageEntries.BinarySearch(speculativeEntry);
+            if (finalSearchResult >= 0) {
+                var foundEntry = pageEntries[finalSearchResult];
+                indexEntry = new IndexEntry {
+                    Index = foundEntry.m_archiveIndex.ToInt(),
+                    Offset = foundEntry.m_offset.ToInt(),
+                    Size = foundEntry.m_size.ToInt()
+                };
+                return true;
+            }
+            
+            //Console.Out.WriteLine($"not found oopsie {pageIndex}: {finalSearchResult}");
+
+            NOT_FOUND:
+            indexEntry = default;
+            return false;
+        }
+
+        public bool TryGetIndexEntry(FullEKey eKey, out IndexEntry indexEntry) {
+            if (TryGetIndexEntryFromGroup(eKey, out indexEntry)) {
+                return true;
+            }
+            
+            return CDNIndexData.TryGetValue(eKey, out indexEntry);
         }
 
         public byte[]? OpenDataFile(IndexEntry entry)
         {
-            var archive = m_client.ConfigHandler.CDNConfig.Archives[entry.Index];
+            var archive = Client.ConfigHandler.CDNConfig.Archives[entry.Index];
 
-            var cdn = (CDNClient) m_client.NetHandle!;
+            var cdn = (CDNClient) Client.NetHandle!;
             var stream = cdn.FetchCDN("data", archive, ((int)entry.Offset, (int)entry.Offset + (int)entry.Size - 1));
             return stream;
         }
 
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        private struct ArchiveGroupEntry : IComparable<ArchiveGroupEntry> {
+            public FullEKey m_eKey;
+            public UInt32BE m_size;
+            public UInt16BE m_archiveIndex;
+            public UInt32BE m_offset;
+
+            public int CompareTo(ArchiveGroupEntry other) {
+                return m_eKey.CompareTo(other.m_eKey);
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
         private struct FixedFooter {
             public byte m_version;
             public byte m_unk0x11;
             public byte m_unk0x12;
-            public byte m_blockSizeKB;
+            public byte m_pageSizeKB;
             public byte m_offsetBytes;
             public byte m_sizeBytes;
             public byte m_keyBytes;
             public byte m_checksumSize;
             public uint m_numElements;
 
+            public int PageSizeBytes => m_pageSizeKB * 1024;
             public int DynamicSize => SIZE + m_checksumSize * 2;
 
             public static unsafe int SIZE => sizeof(FixedFooter);
+            public const byte MIN_CHECKSUM_SIZE = 8;
+            public const byte MAX_CHECKSUM_SIZE = 16;
         }
     }
 }
