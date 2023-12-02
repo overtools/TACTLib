892250e33b5b0e59dbe0851b918dc14193aa5cfe
zingballyhoo <zingballyhoo@live.co.uk>
2023-12-02 19:27:44 +0000

    fix another couple warnings

--- TACTLib/Helpers/UInt16BE.cs
+++ TACTLib/Helpers/UInt16BE.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Buffers.Binary;
+using System.Runtime.InteropServices;
 
 namespace TACTLib.Helpers {
+    [StructLayout(LayoutKind.Sequential)]
     public struct UInt16BE
     {
         private ushort m_data;
--- TACTLib/Helpers/UInt24BE.cs
+++ TACTLib/Helpers/UInt24BE.cs
@@ -1,15 +1,22 @@
-using System.Runtime.CompilerServices;
+using System;
+using System.Runtime.InteropServices;
 
 namespace TACTLib.Helpers
 {
-    [InlineArray(3)]
+    [StructLayout(LayoutKind.Sequential)]
     public struct UInt24BE
     {
-        private byte m_first;
+        private byte m_a;
+        private byte m_b;
+        private byte m_c;
 
         public readonly int ToInt()
         {
-            return this[2] | (this[1] << 8) | (this[0] << 16);
+            if (BitConverter.IsLittleEndian)
+            {
+                return m_c | (m_b << 8) | (m_a << 16);
+            }
+            throw new NotImplementedException();
         }
     }
 }
