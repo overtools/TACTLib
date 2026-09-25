ec82cdbb1a4d76c41a575a926b5863db59d03d58
Hazno21688 <62346467+Hazno-dev@users.noreply.github.com>
2026-09-25 10:10:48 +0100

    Added missing client misc args/info
    Just added the missing platform/region strings for clarity

--- TACTLib/Client/HandlerArgs/ClientCreateArgs_Tank.cs
+++ TACTLib/Client/HandlerArgs/ClientCreateArgs_Tank.cs
@@ -20,17 +20,27 @@ namespace TACTLib.Client.HandlerArgs {
         /// Load manifest files. Flag here to allow disabling for development purposes
         /// </summary>
         public bool LoadManifest { get; set; } = true;
-        
+
         /// <summary>
         /// Load bundles for lookup. Flag here to allow disabling for development purposes
         /// </summary>
         public bool LoadBundlesForLookup { get; set; } = true;
-        
+
         public const string REGION_DEV = "DEV";
         public const string REGION_CN = "CN";
         public const string REGION_KR = "KR";
+        public const string REGION_TW = "TW";
+        public const string REGION_SG = "SG";
+        public const string REGION_EU = "EU";
+        public const string REGION_US = "US";
 
         public const string PLATFORM_WIN = "Win";
         public const string PLATFORM_WINPRISM = "WinPrism";
+        public const string PLATFORM_XBOXONE = "XB1";
+        public const string PLATFORM_XBOXSERIES = "XSX";
+        public const string PLATFORM_PLAYSTATION4 = "PS4";
+        public const string PLATFORM_PLAYSTATION5 = "PS5";
+        public const string PLATFORM_NINTENDOSWITCH = "NX";
+        public const string PLATFORM_NINTENDOSWITCH2 = "NX2";
     }
 }
